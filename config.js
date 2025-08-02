aee41b93a1d3b10f32a3dfcc61cc7523418c3c89
hmawari639 <phn6329@gmail.com>
2025-08-02 13:16:12 +0700

    Update 🌻🍁✨.js

--- config.js
+++ config.js
@@ -1,20 +1,19 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
+    titleWeb: "Gửi Nam Phonggg",
+    introTitle: '🍁🍂',
+    introDesc: `hellooo, 2/8 gòi nè anh có biết hôm nay là ngày gì khomm:))`,
+    btnIntro: ' ýe đúng òi nay là sinh nhật A̶n̶h̶ i̶u̶ Nam Phong đó ^^HiHi^^',
+    title: '18 tuổi rồi, sắp phải ra ngoài var với xã hội gòi:)) nên là chúc gặp nhiều may mắn, bớt đen lại:) với cạ là phải vui vẻ lên nè, mở lòng ra đón nhận, tiếp thu những cái mới mẻ nhe chúc anh thích nghi được tốt với cuộc sống mới phải tự lập ở đại học. Học giỏi nhe i̶u̶',
+    desc: 'Thoát web là phải cố gắng thực hiện tốt mấy lời em chúc nhee',
+    btnYes: 'ogeee✨',
+    btnNo: 'Không🙄',
+    question: 'Trên thế giới hơn 7 tỉ người mà 2 đứa mình lại gặp nhau thía <3',
+    btnReply: 'Gửi',
+    reply: 'đẹt ti ni đó hẹ hẹ🤡🥰:))',
+    mess: 'aww em biết mà✨:))',
+    finalDesc: 'vì cũng có căn dữ lắm mới gặp được nhau nên là dù có thế nào thì cũng không được sủi nhe🌻',
+    btnYes: 'chắc chắn òi:]]',
+    btnNo: 'hên xui😞',
+    messDesc: 'Tối nay, mình chơi game, xem phim nhaaa.',
+    btnAccept: 'Okiiiii lun <3'
 }
